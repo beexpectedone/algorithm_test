4d9dd468184789cb3749afb2ed3e37873e62bfeb
moon_windows_work <wqyfighting@163.com>
2018-11-08 11:31:20 +0800

    快速排序尝试使用high位置作为标兵位

--- src/com/personal/playWithAlg/class1_3/QuickSort.java
+++ src/com/personal/playWithAlg/class1_3/QuickSort.java
@@ -19,22 +19,31 @@ public class QuickSort {
         }
     }
 
+    /**
+     *
+     * @param arr 源数组
+     * @param low 最低位角标
+     * @param high 最高位角标
+     * @return 返回最后标兵位角标
+     *
+     *
+     * 启发：标兵位可以选择 low 位置，也可以选择 high 位置 ，如果选择的是 low 位置的则while循环要先从high执行
+     * 如果选择的是 high 位置，则while循环要从低位开始执行
+     */
     private static int partition(int[] arr, int low, int high) {
-        int pivot = arr[low];  //数轴记录
+        int pivot = arr[high];  //数轴记录
         /** 中间的循环一定要先从high开始，如果从low开始就无法正确排序 */
         while (low < high) {
-            while (low < high && arr[high] >= pivot){
-                --high;
-            }
-            arr[low] = arr[high]; //
             while (low < high && arr[low] <= pivot){ //
                 ++low;
             }
             arr[high] = arr[low];
+            while (low < high && arr[high] >= pivot){
+                --high;
+            }
+            arr[low] = arr[high]; //
         }
-        arr[low] =pivot;
-        return low;
+        arr[high] =pivot;
+        return high;
     }
-
-
 }
