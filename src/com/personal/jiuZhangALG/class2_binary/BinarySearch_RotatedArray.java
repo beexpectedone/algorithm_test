baf05ec01737ae2b340e6e4395fb5d9ef37b5079
moon_wang <wqyfighting@gmail.com>
2018-12-02 00:08:29 +0800

    rotatedArray update

--- src/com/personal/jiuZhangALG/class2_binary/BinarySearch_RotatedArray.java
+++ src/com/personal/jiuZhangALG/class2_binary/BinarySearch_RotatedArray.java
@@ -86,4 +86,64 @@ public class BinarySearch_RotatedArray {
             return nums[end]; //说明此时end是落在target分段上，而start还是在另外一个分段上
         }
     }
+
+    /**
+     * 补充：
+     *      如果在螺旋数组中出现了重复的数，那么以上方法就失效了，它最坏的情况时间复杂度变成了O(n)，
+     *      这个时候使用的二分法也是fake的。
+     *
+     *  例题：
+     *      假设一个旋转排序的数组其起始位置是未知的（比如0 1 2 4 5 6 7 可能变成是4 5 6 7 0 1 2）。
+
+            你需要找到其中最小的元素。
+
+            数组中可能存在重复的元素。
+
+            给出[4,4,5,6,7,0,1,2]  返回 0
+     *
+     *
+     * */
+    public int findMin2(int[] num) {
+        //这道题目在面试中不会让写完整程序
+        //只需要知道最坏情况下,时间复杂度为O(n)
+        //因此这一个for循环就可以了
+        //如果你觉得，不是每个情况都是最坏情况，你想用二分法解决不是最坏情况的情况，那你就可以写一个二分法
+        //面试这道题的目的是为了考你能不能想到最坏情况
+
+        int min = num[0];
+        for (int i = 1; i < num.length; i++) {
+            if (num[i] < min)
+                min = num[i];
+        }
+        return min;
+    }
+
+    //version 2：use  *fake* binary-search
+    public int findMin2_2(int[] num) {
+        if (num == null || num.length == 0){
+            return -1;
+        }
+
+        int start = 0, end = num.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (num[mid] == num[end]) {
+                //if mid equals to end, that means it's fine to remove end
+                //to smallest element won't be removed
+                end--;
+            } else if (num[mid] < num[end]){
+                //of course you can merge == & <
+                end = mid;
+            } else {
+                start = mid;
+                // or start = mid + 1
+            }
+        }
+
+        if (num[start] <= num[end]) {
+            return num[start];
+        }
+
+        return num[end];
+    }
 }
