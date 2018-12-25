b1834b1c871271e29b2ad2b11514e659027f7db1
moon_windows_work <wqyfighting@163.com>
2018-12-25 17:27:38 +0800

    dynamicProgramming1 of triangle test

--- src/com/com/personal/jiuZhangALG/class4_dynamicProgramming1/DynamicProgramming1_triangle_practice.java
+++ src/com/com/personal/jiuZhangALG/class4_dynamicProgramming1/DynamicProgramming1_triangle_practice.java
@@ -1,12 +1,15 @@
 package com.personal.jiuZhangALG.class4_dynamicProgramming1;
 
+
+/**
+ * 感悟：
+ *      转化成找出一个每一位置都是“最短路径”的题，每个位置上的数的值都是到上个位置的最短路径加上
+ *      该位置的triangle值
+ * */
 public class DynamicProgramming1_triangle_practice {
 
 
-
-
-
-    public int minimumTotal_bottom_to_up(int[][] triangle) {
+    public int minimumTotal_top_down(int[][] triangle) {
         //illegal
         if (null == triangle || triangle.length == 0) {
             return -1;
@@ -14,6 +17,37 @@ public class DynamicProgramming1_triangle_practice {
         if (triangle[0] == null || triangle[0].length == 0) {
             return -1;
         }
+
+        int n = triangle.length;
+        int[][] f = new int[n][n];
+        //先把两个边列的值求出来, 最边上的两列的值都只有一种情况
+        f[0][0] = triangle[0][0];
+        for (int i = 1; i < n ; i++) {
+            f[i][0] = triangle[i][0] + f[i - 1][0]; //最左边的一列，行一直在增加，列不变
+            f[i][i] = triangle[i][i] + f[i - 1][i - 1]; //最右边的一列
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < i; j++) { // 这里面比较的是去除了 “最左边”、“最右边”两列的值
+                f[i][j] = Math.min(f[i - 1][j - 1], f[i - 1][j]) + triangle[i][j];
+            }
+        }
+        //answer
+        int best = f[n - 1][0];
+        //最后，比较最后一行的值，获取到整个二维数组中最大的值
+        for (int i = 1; i < n; i++) {
+            best = Math.max(best, f[n -1][i]);
+        }
+        return best;
+    }
+
+    public int minimumTotal_bottom_up(int[][] triangle) {
+        //illegal
+        if (null == triangle || triangle.length == 0) {
+            return -1;
+        }
+        if (triangle[0] == null || triangle[0].length == 0) {
+            return -1;
+        }//
 
         int n = triangle.length;
         int[][] f = new int[n][n]; //初始化最终结果的二维数组
@@ -44,7 +78,9 @@ public class DynamicProgramming1_triangle_practice {
         if (minSum[x][y] != Integer.MAX_VALUE) {
             return minSum[x][y];
         }
-
+        /**
+         * 最后一排位置的值等于triangle最后一排的值
+         * */
         minSum[x][y] = Math.min(memorizeSearch(x + 1, y), memorizeSearch(x + 1, y + 1))
                         + triangle[x][y];
         return minSum[x][y];
@@ -63,7 +99,8 @@ public class DynamicProgramming1_triangle_practice {
         this.n = triangle.length;
         this.triangle = triangle;
         this.minSum = new int[n][n];
-
+        /*先初始化一个每个位置都是
+        Integer.MAX_VALUE值的二维数组*/
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 minSum[i][j] = Integer.MAX_VALUE;
